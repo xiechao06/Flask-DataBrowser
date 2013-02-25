f28dcd1fb674cbaaea5f9ded78f6581dc055446e
abc549825 <abc549825@163.com>
2013-02-25 17:16:27 +0800

    fix the bug

--- flask_databrowser/action.py
+++ flask_databrowser/action.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 from flask.ext.babel import gettext, ngettext
 
-class BaseAction(object):
 
+class BaseAction(object):
     def __init__(self, name):
         self.name = name
 
@@ -10,25 +10,28 @@ class BaseAction(object):
         return ""
 
     def success_message(self, models):
-        try: 
+        try:
             model_name = getattr(models[0], "__modelname__")
         except AttributeError:
             model_name = models[0].__class__.__name__ + ' '
-        return model_name + ",".join(unicode(model) for model in models).join(['[', ']']) + gettext(u"被成功%(name)s", name=self.name)
+        return model_name + ",".join(unicode(model) for model in models).join(
+            ['[', ']']) + gettext(u"被成功%(name)s", name=self.name)
 
     def error_message(self, models):
-        try: 
+        try:
             model_name = getattr(models[0], "__modelname__")
         except AttributeError:
             model_name = models[0].__class__.__name__ + ' '
-        return model_name + ','.join(unicode(model) for model in models).join(['[', ']']) + gettest(u"%(name))s失败", name=self.name)
+        return model_name + ','.join(unicode(model) for model in models).join(
+            ['[', ']']) + gettext(u"%(name))s失败", name=self.name)
 
     def enabled(self, model):
         return True
 
     def disabled_tooltip(self, model):
-        try: 
+        try:
             model_name = getattr(model, "__modelname__")
         except AttributeError:
             model_name = model.__class__.__name__ + ' '
-        return model_name + gettext(u'[%(object)s]不能被%(name)s', object=unicode(model), name=self.name)  
+        return model_name + gettext(u'[%(object)s]不能被%(name)s',
+                                    object=unicode(model), name=self.name)
