d6e91a0ba927116becda9ab6f26706051ee52d6a
xiechao <xiechao06@gmail.com>
2013-02-25 17:11:15 +0800

    trivial

--- flask_databrowser/example/__main__.py
+++ flask_databrowser/example/__main__.py
@@ -74,14 +74,8 @@ def main():
             def op(self, model):
                 model.roll_call()
 
-            def success_message(self, model):
-                return ",".join(user.name for user in model) + u" 点名成功"
-
             def enabled(self, model):
                 return not model.roll_called
-
-            def disabled_tooltip(self, model):
-                return u"id为%d的不能被点名" % model.id
 
         __customized_actions__ = [RollCall(u"点名")]
 
