69e56be292f4f97c1add5858d72742e9ab8dd1d9
abc549825 <abc549825@163.com>
2013-08-12 17:00:12 +0800

    fix #273

--- flask_databrowser/test/test_create_api.py
+++ flask_databrowser/test/test_create_api.py
@@ -8,11 +8,11 @@ from flask.ext.databrowser.test import basetest
 from flask.ext.databrowser import ModelView, DataBrowser
 from flask.ext.databrowser.column_spec import InputColumnSpec
 
+
 class TestCreateAPI(basetest.BaseTest):
-
     def setup_models(self):
-
         db = self.db
+
         class Group(db.Model):
             __tablename__ = "TB_GROUP"
 
@@ -38,7 +38,6 @@ class TestCreateAPI(basetest.BaseTest):
 
         self.__tables = namedtuple("foo", ["User", "Group"])(User, Group)
 
-
     def setup(self):
         super(TestCreateAPI, self).setup()
         self.browser = DataBrowser(self.app, self.db)
@@ -51,12 +50,11 @@ class TestCreateAPI(basetest.BaseTest):
         self.db.session.add(user1)
         self.db.session.add(user2)
         self.db.session.commit()
-    
+
     def test_with_no_create_columns(self):
         # we assert when no create columns are defined, creat api return correctly
         model_view = ModelView(self.__tables.User)
-        blueprint = Blueprint("foo0", __name__, static_folder="static", 
-                            template_folder="templates")
+        blueprint = Blueprint("foo0", __name__, static_folder="static", template_folder="templates")
         self.browser.register_model_view(model_view, blueprint)
         self.app.register_blueprint(blueprint, url_prefix="/foo0")
 
@@ -120,16 +118,15 @@ class TestCreateAPI(basetest.BaseTest):
                 assert options['Ms'] == 'Ms'
                 assert options['Mr'] == 'Mr'
                 assert options['Mrs'] == 'Mrs'
-    
+
     def test_not_hidden_pk(self):
         ## we assert primary key isn't hidden when we meant to
         class UserModelView(ModelView):
-
             hidden_pk = False
 
         model_view = UserModelView(self.__tables.User)
-        blueprint = Blueprint("foo1", __name__, static_folder="static", 
-                            template_folder="templates")
+        blueprint = Blueprint("foo1", __name__, static_folder="static",
+                              template_folder="templates")
         self.browser.register_model_view(model_view, blueprint)
         self.app.register_blueprint(blueprint, url_prefix="/foo1")
 
@@ -147,12 +144,11 @@ class TestCreateAPI(basetest.BaseTest):
     def test_many_to_one(self):
         ## we test many to one relationship
         class GroupModelView(ModelView):
-
             column_hide_backrefs = False
 
         model_view = GroupModelView(self.__tables.Group)
-        blueprint = Blueprint("foo2", __name__, static_folder="static", 
-                            template_folder="templates")
+        blueprint = Blueprint("foo2", __name__, static_folder="static",
+                              template_folder="templates")
         self.browser.register_model_view(model_view, blueprint)
         self.app.register_blueprint(blueprint, url_prefix="/foo2")
         with self.app.test_request_context():
@@ -164,7 +160,7 @@ class TestCreateAPI(basetest.BaseTest):
                 assert 'name' in fields
                 assert 'users' in fields
 
-                field = fields['users'] 
+                field = fields['users']
                 assert field['type'] == 'select'
                 assert len(field['options']) == 2
                 options = dict(field['options'])
@@ -173,12 +169,11 @@ class TestCreateAPI(basetest.BaseTest):
 
     def test_extra_validators(self):
         class UserModelView(ModelView):
-
             __create_columns__ = [InputColumnSpec("age", validators=[validators.NumberRange(18, 101)])]
 
         model_view = UserModelView(self.__tables.User)
-        blueprint = Blueprint("foo3", __name__, static_folder="static", 
-                            template_folder="templates")
+        blueprint = Blueprint("foo3", __name__, static_folder="static",
+                              template_folder="templates")
         self.browser.register_model_view(model_view, blueprint)
         self.app.register_blueprint(blueprint, url_prefix="/foo3")
         with self.app.test_request_context():
@@ -196,28 +191,29 @@ class TestCreateAPI(basetest.BaseTest):
                 assert validator['name'] == 'range'
                 assert validator['min'] == 18
                 assert validator['max'] == 101
-        
-        # TODO more validators should be tested here
 
-    
+                # TODO more validators should be tested here
+
+
     def test_many_to_many(self):
         # TODO untested
         pass
 
     def test_commit(self):
         class UserModelView(ModelView):
-            __create_columns__ = ['name', 'title', 'group', InputColumnSpec("age", validators=[validators.NumberRange(18, 101)])]
+            __create_columns__ = ['name', 'title', 'group',
+                                  InputColumnSpec("age", validators=[validators.NumberRange(18, 101)])]
 
         model_view = UserModelView(self.__tables.User)
-        blueprint = Blueprint("foo5", __name__, static_folder="static", 
-                            template_folder="templates")
+        blueprint = Blueprint("foo5", __name__, static_folder="static",
+                              template_folder="templates")
         self.browser.register_model_view(model_view, blueprint)
         self.app.register_blueprint(blueprint, url_prefix="/foo5")
         with self.app.test_request_context():
             with self.app.test_client() as c:
-                data =  {
+                data = {
                     "name": "foo",
-                    "title": "Mr", 
+                    "title": "Mr",
                     "group": '1',
                     "age": 22,
                 }
@@ -241,9 +237,8 @@ class TestCreateAPI(basetest.BaseTest):
                 assert 'group' in rsp['errors']
                 assert 'age' in rsp['errors']
 
-        self.db.session.delete(self.__tables.User.query.filter(self.__tables.User.name=='foo').one())
+        self.db.session.delete(self.__tables.User.query.filter(self.__tables.User.name == 'foo').one())
         self.db.session.commit()
-
 
 
 if __name__ == "__main__":
--- flask_databrowser/test/test_normalize.py
+++ flask_databrowser/test/test_normalize.py
@@ -7,12 +7,12 @@ from flask.ext.databrowser.test import basetest
 from flask.ext.databrowser import ModelView
 from flask.ext.databrowser.column_spec import InputColumnSpec, PlaceHolderColumnSpec, TableColumnSpec
 
+
 class TestNormalize(basetest.BaseTest):
-
-
     def setup_models(self):
 
         db = self.db
+
         class Group(db.Model):
             __tablename__ = "TB_GROUP"
 
@@ -41,27 +41,27 @@ class TestNormalize(basetest.BaseTest):
 
         col_spec = columns["id"]
         assert col_spec.col_name == "id"
-        assert col_spec.label is None 
+        assert col_spec.label is None
         assert col_spec.doc is None
         assert col_spec.property_ == self.__tables.User.id.property
 
         col_spec = columns["name"]
         assert col_spec.col_name == "name"
-        assert col_spec.label is None 
+        assert col_spec.label is None
         assert col_spec.doc == u"姓名"
         assert col_spec.property_ == self.__tables.User.name.property
 
         col_spec = columns["group"]
         assert col_spec.col_name == "group"
-        assert col_spec.label is None 
+        assert col_spec.label is None
         assert col_spec.doc is None
         assert col_spec.property_ == self.__tables.User.group.property
 
         class UserModelView(ModelView):
-            
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
-        
+
         model_view = UserModelView(self.__tables.User)
         assert len(model_view.create_columns) == 1
         columns = dict((col_spec.col_name, col_spec) for col_spec in model_view.create_columns[""])
@@ -84,9 +84,9 @@ class TestNormalize(basetest.BaseTest):
 
         # we assert that the columns not defined in model are purged
         class UserModelView(ModelView):
-            
+
             __create_columns__ = ["id", PlaceHolderColumnSpec("name", template_fname="foo.html"), "field_inexistent"]
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
 
         model_view = UserModelView(self.__tables.User)
@@ -103,9 +103,10 @@ class TestNormalize(basetest.BaseTest):
         # we assert that the input column spec and place holder column spec are 
         # normalized
         class UserModelView(ModelView):
-            
-            __create_columns__ = [InputColumnSpec("id"), PlaceHolderColumnSpec("name", template_fname="foo.html", as_input=True)]
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+
+            __create_columns__ = [InputColumnSpec("id"),
+                                  PlaceHolderColumnSpec("name", template_fname="foo.html", as_input=True)]
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
 
         model_view = UserModelView(self.__tables.User)
@@ -123,10 +124,11 @@ class TestNormalize(basetest.BaseTest):
         assert col_spec1.property_ == self.__tables.User.name.property
 
         class UserModelView(ModelView):
-            
-            __create_columns__ = [InputColumnSpec("id", label="id", doc="identity"), 
-                                  PlaceHolderColumnSpec("name", label="name", doc="username", template_fname="foo.html", as_input=True)]
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+
+            __create_columns__ = [InputColumnSpec("id", label="id", doc="identity"),
+                                  PlaceHolderColumnSpec("name", label="name", doc="username",
+                                                        template_fname="foo.html", as_input=True)]
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
 
         model_view = UserModelView(self.__tables.User)
@@ -150,9 +152,9 @@ class TestNormalize(basetest.BaseTest):
         create_columns["secondary"] = [PlaceHolderColumnSpec("name", template_fname="foo.html", as_input=True)]
 
         class UserModelView(ModelView):
-            
+
             __create_columns__ = create_columns
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
 
         model_view = UserModelView(self.__tables.User)
@@ -188,27 +190,27 @@ class TestNormalize(basetest.BaseTest):
 
         col_spec = columns["id"]
         assert col_spec.col_name == "id"
-        assert col_spec.label is None 
+        assert col_spec.label is None
         assert col_spec.doc is None
         assert col_spec.property_ == self.__tables.User.id.property
 
         col_spec = columns["name"]
         assert col_spec.col_name == "name"
-        assert col_spec.label is None 
+        assert col_spec.label is None
         assert col_spec.doc == u"姓名"
         assert col_spec.property_ == self.__tables.User.name.property
 
         col_spec = columns["group"]
         assert col_spec.col_name == "group"
-        assert col_spec.label is None 
+        assert col_spec.label is None
         assert col_spec.doc is None
         assert col_spec.property_ == self.__tables.User.group.property
 
         class UserModelView(ModelView):
-            
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
-        
+
         model_view = UserModelView(self.__tables.User)
         assert len(model_view.form_columns) == 1
         columns = dict((col_spec.col_name, col_spec) for col_spec in model_view.form_columns[""])
@@ -231,9 +233,9 @@ class TestNormalize(basetest.BaseTest):
 
         # we assert that the columns not defined in model are purged
         class UserModelView(ModelView):
-            
+
             __form_columns__ = ["id", PlaceHolderColumnSpec("name", template_fname="foo.html"), "field_inexistent"]
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
 
         model_view = UserModelView(self.__tables.User)
@@ -250,9 +252,10 @@ class TestNormalize(basetest.BaseTest):
         # we assert that the input column spec and place holder column spec are 
         # normalized
         class UserModelView(ModelView):
-            
-            __form_columns__ = [InputColumnSpec("id"), PlaceHolderColumnSpec("name", template_fname="foo.html", as_input=True)]
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+
+            __form_columns__ = [InputColumnSpec("id"),
+                                PlaceHolderColumnSpec("name", template_fname="foo.html", as_input=True)]
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
 
         model_view = UserModelView(self.__tables.User)
@@ -270,10 +273,11 @@ class TestNormalize(basetest.BaseTest):
         assert col_spec1.property_ == self.__tables.User.name.property
 
         class UserModelView(ModelView):
-            
-            __form_columns__ = [InputColumnSpec("id", label="id", doc="identity"), 
-                                  PlaceHolderColumnSpec("name", label="name", doc="username", template_fname="foo.html", as_input=True)]
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+
+            __form_columns__ = [InputColumnSpec("id", label="id", doc="identity"),
+                                PlaceHolderColumnSpec("name", label="name", doc="username", template_fname="foo.html",
+                                                      as_input=True)]
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
 
         model_view = UserModelView(self.__tables.User)
@@ -296,9 +300,9 @@ class TestNormalize(basetest.BaseTest):
         form_columns["secondary"] = [PlaceHolderColumnSpec("name", template_fname="foo.html", as_input=True)]
 
         class UserModelView(ModelView):
-            
+
             __form_columns__ = form_columns
-            __column_labels__ = {"id": u"号码", "name": u"姓名"} 
+            __column_labels__ = {"id": u"号码", "name": u"姓名"}
             __column_docs__ = {"id": u"编号", "name": u"名称"}
 
         model_view = UserModelView(self.__tables.User)
@@ -324,8 +328,9 @@ class TestNormalize(basetest.BaseTest):
 
         # we assert that other column specification like TableColumnSpec are reserved
         class GroupModelView(ModelView):
-            
-            __form_columns__ = [TableColumnSpec("users", css_class="table table-striped table-hover table-condensed table-bordered")]
+
+            __form_columns__ = [
+                TableColumnSpec("users", css_class="table table-striped table-hover table-condensed table-bordered")]
             __column_labels__ = {'users': u"用户列表"}
             __column_docs__ = {'users': u"所属用户"}
             column_hide_backrefs = False
@@ -335,6 +340,7 @@ class TestNormalize(basetest.BaseTest):
         assert isinstance(col_spec0, TableColumnSpec)
         assert col_spec0.label == u"用户列表"
         assert col_spec0.doc == u"所属用户"
+
 
 if __name__ == "__main__":
     TestNormalize().run_plainly()
