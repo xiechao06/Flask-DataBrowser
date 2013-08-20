ef8d1a1c5f7aabcceb2751a23668b38f6b535155
abc549825 <abc549825@163.com>
2013-08-20 12:44:25 +0800

    revert

--- flask_databrowser/base.py
+++ flask_databrowser/base.py
@@ -848,8 +848,7 @@ class ModelView(object):
         if form.is_submitted():
             # alas! something wrong
             resp = make_response(resp, 403)
-            # resp.headers["Warning"] = u"&".join([k + u"-" + u"; ".join(v) for k, v in form.errors.items()]).encode("utf-8")
-            print form.errors
+            resp.headers["Warning"] = u"&".join([k + u"-" + u"; ".join(v) for k, v in form.errors.items()]).encode("utf-8")
         return resp
 
     def get_create_help(self):
