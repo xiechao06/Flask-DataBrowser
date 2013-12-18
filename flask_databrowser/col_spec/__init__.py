f0098abe3ab4012e39ff6079d2bf7c9c8f32b8b5
xiechao06 <xiechao06@gmail.com>
2013-12-18 14:22:27 +0800

    foo

--- flask_databrowser/col_spec/__init__.py
+++ flask_databrowser/col_spec/__init__.py
@@ -6,11 +6,11 @@ from .html_snippet_col_spec import HtmlSnippetColSpec
 from .input_html_snippet_col_spec import InputHtmlSnippetColSpec
 from .file_col_spec import FileColSpec
 
-from .image_column_spec import ImageColumnSpec
-from .link_column_spec import LinkColumnSpec
-from .table_column_spec import TableColumnSpec
-from .list_column_spec import ListColumnSpec
-from .place_holder_column_spec import PlaceHolderColumnSpec
-from .input_place_holder_column_spec import InputPlaceHolderColumnSpec
-from .select_column_spec import SelectColumnSpec
-from .file_column_spec import FileColumnSpec
+#from .image_column_spec import ImageColumnSpec
+#from .link_column_spec import LinkColumnSpec
+#from .table_column_spec import TableColumnSpec
+#from .list_column_spec import ListColumnSpec
+#from .place_holder_column_spec import PlaceHolderColumnSpec
+#from .input_place_holder_column_spec import InputPlaceHolderColumnSpec
+#from .select_column_spec import SelectColumnSpec
+#from .file_column_spec import FileColumnSpec
